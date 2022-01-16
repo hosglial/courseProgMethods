77cd55dc680c8fe66744303e322498a11c421301
Alexey <hosglial@gmail.com>
2022-01-16 22:57:49 +0300

    added new logic

--- graph.py
+++ graph.py
@@ -103,68 +103,6 @@ class App:
         mbox.setStandardButtons(QMessageBox.Ok)
         mbox.exec_()
 
-    def init_graphs(self):
-        # инициализация импортированного графа
-        self.imported_graph.init_graph(self.imported_edges)
-
-        self.cleared_graph.graph.clear()
-        self.eq_graph.graph.clear()
-
-        source_sub_edges = []
-        # построение массива подграфов, подключенных к источникам
-        for source_node in self.source_nodes:
-            source_sub_edges.append(list(nx.dfs_edges(self.imported_graph.graph, source_node)))
-
-        print(source_sub_edges)
-
-        return
-
-        self.weight_cleared_nodes.clear()
-        for node in self.weight_nodes.keys():
-            if node in self.cleared_graph.graph.nodes:
-                self.weight_cleared_nodes[node] = self.weight_nodes[node]
-
-        # эквивалентирование
-        tr = Tree()
-        for e1, e2 in self.cleared_edges:
-            if e1 not in tr.nodes:
-                tr.create_node(e1, e1, data=self.weight_cleared_nodes[e2] if e2 in self.weight_cleared_nodes else 0)
-            tr.create_node(e2, e2, parent=e1,
-                           data=self.weight_cleared_nodes[e2] if e2 in self.weight_cleared_nodes else 0)
-
-        tr_dict = tr.to_dict(with_data=True)
-
-        self.count_edges(tr_dict)
-
-        self.take_nodes(tr_dict)
-
-        self.counted_edges.clear()
-        for n1, n2 in self.cleared_edges:
-            if self.counted_nodes.get(n2, 0) > 0:
-                self.counted_edges.append((n1, n2, self.counted_nodes.get(n2, 0)))
-
-        self.eq_graph.init_graph(self.counted_edges)
-
-        self.drop_unweighted(self.eq_graph.graph)
-
-        self.ui.button_source.setEnabled(True)
-        self.ui.excel_button_3.setEnabled(True)
-        self.ui.button_cleared.setEnabled(True)
-
-    def drop_unweighted(self, graph):
-        all_nodes = set(graph.nodes)
-        while len(all_nodes) > 0:
-            fnodes = list(
-                filter(
-                    lambda x: nx.degree(graph,
-                                        x) == 1 and x not in self.source_node and x not in self.weight_cleared_nodes.keys(),
-                    graph.nodes))
-            for node in fnodes:
-                graph.remove_node(node)
-            if len(fnodes) == 0:
-                break
-        return graph
-
     def import_data(self):
         # импортировать данные с excel
         dialog = QFileDialog()
@@ -189,16 +127,11 @@ class App:
                 return
 
             # заполнение нод, имеющих вес
-            for node, weight, source in zip(self.df_nodes['weighted_nodes'], self.df_nodes['weight'],
+            for node, weight, source in zip(self.df_nodes['name'], self.df_nodes['weight'],
                                             self.df_nodes['source']):
                 self.weight_nodes[node] = weight
                 if not pd.isnull(source):
                     self.source_nodes.append(node)
-
-            print(self.source_nodes)
-
-
-
 
         except (AttributeError, KeyError, ValueError):
             self.drop_error('Incorrect file')
@@ -206,19 +139,103 @@ class App:
 
         self.init_graphs()
 
+    def init_graphs(self):
+        # инициализация импортированного графа
+        self.imported_graph.init_graph(self.imported_edges)
+
+        self.cleared_graph.graph.clear()
+        self.eq_graph.graph.clear()
+
+        source_sub_edges = []
+        # построение массива подграфов, подключенных к источникам
+        for source_node in self.source_nodes:
+            source_sub_edges.append(list(nx.dfs_edges(self.imported_graph.graph, source_node)))
+
+        # print(source_sub_edges)
+
+        node_info = {
+            row['name']: {
+                'weight': row['weight'],
+                'source': row['source']
+            } for index, row in self.df_nodes.iterrows()
+        }
+
+        # итерация по именам точек источников
+        source_nodes = list(self.df_nodes.loc[self.df_nodes['source'] == 1]['name'])
+        for source in source_nodes:
+            # sub_edges =
+            # print(list(sub_edges))
+            tr = Tree()
+
+            for e1, e2 in nx.dfs_edges(self.imported_graph.graph, source):
+                if e1 not in tr.nodes:
+                    tr.create_node(e1, e1, data=node_info.get(e2, 0))
+                tr.create_node(e2, e2, parent=e1, data=node_info.get(e2, 0))
+
+            print(tr)
+            tr_dict = tr.to_dict(with_data=True)
+            self.count_edges(tr_dict)
+
+            self.counted_nodes.clear()
+            self.take_nodes(tr_dict)
+
+            print(tr_dict[list(tr_dict.keys())[0]]['data']['weight'])
+
+        self.ui.button_source.setEnabled(True)
+        self.ui.excel_button_3.setEnabled(True)
+        self.ui.button_cleared.setEnabled(True)
+
+        return
+
+        # self.weight_cleared_nodes.clear()
+        # for node in self.weight_nodes.keys():
+        #     if node in self.cleared_graph.graph.nodes:
+        #         self.weight_cleared_nodes[node] = self.weight_nodes[node]
+        #
+        # # эквивалентирование
+
+        #
+
+        #
+        # self.take_nodes(tr_dict)
+        #
+        # self.counted_edges.clear()
+        # for n1, n2 in self.cleared_edges:
+        #     if self.counted_nodes.get(n2, 0) > 0:
+        #         self.counted_edges.append((n1, n2, self.counted_nodes.get(n2, 0)))
+        #
+        # self.eq_graph.init_graph(self.counted_edges)
+        #
+        # self.drop_unweighted(self.eq_graph.graph)
+
+    def drop_unweighted(self, graph):
+        all_nodes = set(graph.nodes)
+        while len(all_nodes) > 0:
+            fnodes = list(
+                filter(
+                    lambda x: nx.degree(graph,
+                                        x) == 1 and x not in self.source_node and x not in self.weight_cleared_nodes.keys(),
+                    graph.nodes))
+            for node in fnodes:
+                graph.remove_node(node)
+            if len(fnodes) == 0:
+                break
+        return graph
+
     def count_edges(self, node) -> None:
+        node_name = node
         if not isinstance(node, dict):
             node = {node: {}}
 
         node = node[list(node.keys())[0]]
-
         if node.get('children'):
             for child in node['children']:
-                self.count_edges(child)
-            try:
-                node['data'] += sum(child[list(child.keys())[0]]['data'] for child in node['children'])
-            except KeyError:
-                print(node)
+                if child[list(child.keys())[0]]['data']['source'] != 1:
+                    self.count_edges(child)
+
+            # print(sum(child[list(child.keys())[0]]['data']['weight'] for child in node['children']))
+            node['data']['weight'] += sum(child[list(child.keys())[0]]['data']['weight'] for child in node['children'])
+            # print(node_name, node['data']['weight'])
 
     def take_nodes(self, node):
         node_data = node[list(node.keys())[0]]
